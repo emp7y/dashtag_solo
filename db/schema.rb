c1e2589283daf24240117d062eb2e5a80019c7c3
adhulip <rudysback@gmail.com>
2015-01-31 13:33:57 -0600

    #1 | make app deployable to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150129002022) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dashtag_posts", force: :cascade do |t|
     t.string   "media_url"
     t.string   "screen_name"
